125ec4a0f0965459fb4b37102ca14106a1d879b8
Thomas Eitrheim <teitrheim@gmail.com>
2023-12-01 23:53:03 +0100

    working on the posts

--- Scripts/feed.js
+++ Scripts/feed.js
@@ -2,20 +2,31 @@ import { fetchPosts } from "./api.js";
 import { logout } from "./auth.js";
 
 document.getElementById("logout-button").addEventListener("click", () => {
+  console.log("Page Loaded");
   logout();
 });
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const token = localStorage.getItem("jwtToken");
-  if (!token) {
-    window.location.href = "../";
-    return;
-  }
-  try {
-    const posts = await fetchPosts();
-    displayPosts(posts);
-  } catch (error) {
-    console.error("Error while fetching posts:", error);
+  const urlParams = new URLSearchParams(window.location.search);
+  const view = urlParams.get("view");
+  const postId = urlParams.get("postId");
+  console.log("View:", view, "Post ID:", postId);
+  if (view === "post" && postId) {
+    try {
+      console.log("Fetching post by ID:", postId);
+      const post = await fetchPostById(postId);
+      displayPost(post);
+    } catch (error) {
+      console.error("Error fetching specific post:", error);
+    }
+  } else {
+    try {
+      console.log("Fetching all posts");
+      const posts = await fetchPosts();
+      displayPosts(posts);
+    } catch (error) {
+      console.error("Error while fetching posts:", error);
+    }
   }
 });
 
@@ -24,12 +35,15 @@ function displayPosts(posts) {
   postFeed.innerHTML = "";
 
   posts.forEach((post) => {
-    // Check if post data is valid before creating the element
     if (post && post.title && post.body) {
       const postElement = document.createElement("div");
       postElement.className = "card mb-3";
+      let mediaHtml = post.media
+        ? `<img src="${post.media}" class="card-img-top">`
+        : "";
       postElement.innerHTML = `
-        <div class="card-body">
+        ${mediaHtml}
+        <div class="card-body" onclick="location.href='/feed?postId=${post.id}'" style="cursor: pointer;">
           <h5 class="card-title">${post.title}</h5>
           <p class="card-text">${post.body}</p>
         </div>
@@ -37,4 +51,45 @@ function displayPosts(posts) {
       postFeed.appendChild(postElement);
     }
   });
+}
+
+async function fetchPostById(postId) {
+  const token = localStorage.getItem("jwtToken");
+  try {
+    const response = await fetch(
+      `https://api.noroff.dev/api/v1/social/posts/${postId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch post: " + response.statusText);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching post by ID:", error);
+    throw error;
+  }
+}
+
+function displayPost(post) {
+  const postFeed = document.getElementById("post-feed");
+  let mediaHtml = post.media
+    ? `<img src="${post.media}" class="card-img-top">`
+    : "";
+  postFeed.innerHTML = `
+    ${mediaHtml}
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">${post.title}</h5>
+        <p class="card-text">${post.body}</p>
+      </div>
+    </div>
+  `;
 }
