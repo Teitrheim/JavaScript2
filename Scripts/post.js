118bd09278098908400f2a3a772835458156318b
Thomas Eitrheim <teitrheim@gmail.com>
2023-12-06 00:01:33 +0100

    working on the like button

--- Scripts/post.js
+++ Scripts/post.js
@@ -1,5 +1,6 @@
 // post.js
 import { fetchPostById } from "./api.js";
+import { API_COMMENTS_URL } from "./common.js";
 
 async function displaySinglePost() {
   console.log("DisplaySinglePost function called");
@@ -75,3 +76,89 @@ function updateLikesDislikes() {
     console.error("Like/Dislike elements not found");
   }
 }
+
+document
+  .getElementById("comment-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const commentText = document.getElementById("comment-text").value;
+    const urlParams = new URLSearchParams(window.location.search);
+    const postId = urlParams.get("postId");
+
+    try {
+      const response = await fetch(API_COMMENTS_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+        },
+        body: JSON.stringify({ postId, body: commentText }),
+      });
+
+      if (response.ok) {
+        const newComment = await response.json();
+        displayComment(newComment);
+        document.getElementById("comment-text").value = "";
+      } else {
+        // Handle errors
+        console.error("Failed to submit comment");
+      }
+    } catch (error) {
+      console.error("Error submitting comment:", error);
+    }
+  });
+
+function displayComment(comment) {
+  const commentsContainer = document.getElementById("comments-container");
+  const commentElement = document.createElement("div");
+  commentElement.className = "comment";
+  commentElement.innerHTML = `
+    <p>${comment.body}</p>
+    <!-- Add more details about the comment here -->
+  `;
+  commentsContainer.appendChild(commentElement);
+}
+async function submitComment(postId, commentBody) {
+  const token = localStorage.getItem("jwtToken");
+  try {
+    const response = await fetch(
+      `https://api.noroff.dev/api/v1/social/posts/${postId}/comment`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ body: commentBody }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Failed to submit comment:", errorData.message);
+      throw new Error("Failed to submit comment: " + response.statusText);
+    }
+
+    const newComment = await response.json();
+    console.log("Comment submitted:", newComment);
+  } catch (error) {
+    console.error("Failed to submit comment", error);
+  }
+}
+
+// Event listener for the comment submission
+document
+  .querySelector("#comment-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const commentInput = document.querySelector("#comment-text");
+    const urlParams = new URLSearchParams(window.location.search);
+    const postId = urlParams.get("postId");
+    console.log("Submitting comment for post ID:", postId);
+    if (commentInput.value.trim() && postId) {
+      await submitComment(postId, commentInput.value.trim());
+      commentInput.value = "";
+    } else {
+      console.error("Post ID or comment text is missing");
+    }
+  });
